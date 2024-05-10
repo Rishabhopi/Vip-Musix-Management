a8047516e70ed622c337751781030f9f99774f12
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-05-10 11:27:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,15 +33,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SH251204/Musix-Management",
+    "https://github.com/Rishabhopi/Vip-Musix-Management",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Unconditional_Dps")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Flames_xD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Ur_rishu_143")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Ur_support07")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "100"))
